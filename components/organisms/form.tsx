cb6079cdf06ad58bb474b7c7fa866eb95d85da3e
nogElBoss <82379815+nogElBoss@users.noreply.github.com>
2022-12-06 16:08:09 +0000

    form com heading

--- components/organisms/form.tsx
+++ components/organisms/form.tsx
@@ -1,4 +1,4 @@
-import { Input, InputGroup, } from '@chakra-ui/react'
+import { Heading, Input, InputGroup, } from '@chakra-ui/react'
 import Buttons from '../atoms/button'
 import Inputs from "../atoms/input"
 
@@ -11,12 +11,16 @@ type InputType = {
 
 function Forms({ margin }: InputType) {
     return (
-        <form action="/api/signup" method="post">
-            <Inputs width="full" color="white" placeholder="NAME" type='text' margin={margin} />
-            <Inputs width="full" color="white" placeholder="EMAIL" type="email" margin={margin} />
-            <Inputs width="full" color="white" placeholder="MESSAGE" type="text" margin={margin} />
-            <Buttons label="SEND MESSAGE" width="full" />
-        </form>
+        <>
+            <Heading textAlign="center" fontFamily="Bodoni MT" color="white" mb="10px" fontSize="40px">
+                CONTACT US
+            </Heading><form action="/api/signup" method="post">
+                <Inputs width="full" color="white" placeholder="NAME" type='text' margin={margin} />
+                <Inputs width="full" color="white" placeholder="EMAIL" type="email" margin={margin} />
+                <Inputs width="full" color="white" placeholder="MESSAGE" type="text" margin={margin} />
+                <Buttons label="SEND MESSAGE" width="full" />
+            </form>
+        </>
     )
 
 
